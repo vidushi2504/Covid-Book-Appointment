948012b0dbeebffc6aaa16d8a6a0760d436fb46a
vidushi2504 <49758900+vidushi2504@users.noreply.github.com>
2021-09-02 15:05:43 +0530

    Update settings.py

--- bookappointment/settings.py
+++ bookappointment/settings.py
@@ -129,8 +129,7 @@ EXEMPT_FOR_NON_AUTHENTICATED_URLS=["/stores/editdetails"]
 
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-#Add the following
-#EMAIL_HOST_USER
-#EMAIL_HOST_PASSWORD
+EMAIL_HOST_USER=os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASS')
 EMAIL_PORT=587
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
